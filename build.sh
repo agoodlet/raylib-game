21743411066c2a9e4541ac8023c3dcbc5f15f70c
Aadyn Goodlet <ajgoodlet@gmail.com>
2024-10-24 20:48:17 +1000

    modify build script to place output in build dir

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -e
 cc -g -std=c99 -c main.c -o ./obj/main.o
 cc -g -std=c99 -c component.c -o ./obj/component.o
-cc -o main obj/main.o obj/component.o -s -Wall -std=c99 -lraylib -lm -lpthread -ldl -lrt -lX11
-./main
+cc -o ./build/main obj/main.o obj/component.o -s -Wall -std=c99 -lraylib -lm -lpthread -ldl -lrt -lX11
+./build/main
